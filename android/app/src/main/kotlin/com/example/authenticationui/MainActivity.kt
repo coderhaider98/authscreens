1cdbe8febea0b483a8ee3799a28ab11d78b7a7b2
DELL <coderhaider98@gmail.com>
2024-04-04 22:13:56 +0500

    second comit

---
+++ android/app/src/main/kotlin/com/example/authenticationui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.authenticationui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/reset_pass.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'theme.dart';
+import '/widgets/primary_buttons.dart';
+import '/widgets/reset_form.dart';
+
+class ResetPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: kDefaultPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 250,
+            ),
+            Text(
+              'Reset Password',
+              style: titleText,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Please enter your email address',
+              style: subTitle.copyWith(fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ResetForm(),
+            SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LogInScreen(),
+                      ));
+                },
+                child: PrimaryButton(buttonText: 'Reset Password')),
+          ],
+        ),
+      ),
+    );
+  }
+}
